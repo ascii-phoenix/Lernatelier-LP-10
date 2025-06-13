6872a43aab57ac56671da6632f54da1c10071598
Eloi A. Knecht <142593414+ascii-phoenix@users.noreply.github.com>
2025-06-13 12:16:56 +0200

    Add files via upload

---
+++ BiliardLogic.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Billiards
+{
+    public class BiliardLogic
+    {
+        const float BallToTableratioLength = 44;
+        const float BallToTableratioHigth = 22;// B;1226 px , H;673 px // 20.4
+
+        public BiliardLogic() { }
+    }
+}
